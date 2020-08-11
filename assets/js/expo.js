60e77689c4815a62e177385a76ea5fe9a0b80faf
Md Sanaul Haque Shanto <68410401+partridgelab@users.noreply.github.com>
2020-08-12 05:08:27 +0600

    expoo

---
+++ assets/js/expo.js
@@ -0,0 +1,32 @@
+
+var independent_tv = new Clappr.Player({source: "https://itpolly.iptv.digijadoo.net/live/independent_tv/chunks.m3u8", parentId: "#independent_tv"});
+
+var gazi_tv = new Clappr.Player({source: "http://itpolly.iptv.digijadoo.net/live/gazi_tv/chunks.m3u8", parentId: "#gazi_tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
+
+var tv = new Clappr.Player({source: "tv", parentId: "#tv"});
